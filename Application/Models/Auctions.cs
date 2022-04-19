08fd7e9a8333915a42b59b128eb29a30fdd3b40c
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-04-19 16:15:28 -0400

    create AuctionService

--- Application/Models/Auctions.cs
+++ Application/Models/Auctions.cs
@@ -2,5 +2,11 @@ namespace Application.Models;
 
 public class Auctions
 {
-    
+    public  int Id { get; set; }
+    public Double StartingPrice { get; set; }
+    public int MaxSeats => 5;
+    public DateTime StartDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
--- Application/Services/AuctionService.cs
+++ Application/Services/AuctionService.cs
@@ -1,8 +1,39 @@
+using System;
 using Application.Models;
 namespace Application.Services;
 
-
-public class AuctionService
+public interface IAuctionService
 {
+    IList<Auctions> Auctions();
+}
+public class AuctionService : IAuctionService 
+{
+    public IList<Auctions> Auctions()
+    {
+        DateTime eventDate = DateTime.Now.AddDays(5);
+        
+        return Enumerable.Range(1, 5).Select(index => new Auctions
+        {
+            Id = RandomNumber(),
+            StartingPrice = RandomPrice(500.00, 1000.00),
+            StartDate = eventDate,
+            ExpirationDate = eventDate.AddDays(RandomNumber()),
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now.AddDays(index),
+            
+            
+        })
+        .ToList();
+    }
+    private int RandomNumber()
+    {
+        return new Random().Next(100);
+    }
+    
+    private double RandomPrice(double min, double max)
+    {
+        Random random = new Random();
+        return random.NextDouble() * (max - min) + min;
+    }
     
 }
